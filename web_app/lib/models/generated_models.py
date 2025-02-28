f252e434e26cd9afbe800e8d46dd80a858a1d3ac
jeancarson <jeanloucarson@gmail.com>
2025-02-28 17:41:23 +0000

    start of schema fix

--- web_app/lib/models/generated_models.py
+++ web_app/lib/models/generated_models.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from sqlalchemy import DateTime, ForeignKey, Integer, LargeBinary, Numeric, String, Text
+from sqlalchemy import DateTime, ForeignKey, Integer, Numeric, String, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 import datetime
 import decimal
@@ -9,24 +9,15 @@ class Base(DeclarativeBase):
     pass
 
 
-class Devices(Base):
-    __tablename__ = 'devices'
+class Aggregators(Base):
+    __tablename__ = 'aggregators'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
-    uuid: Mapped[str] = mapped_column(Text, unique=True)
+    aggregator_id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
+    aggregator_uuid: Mapped[str] = mapped_column(Text)
+    name: Mapped[str] = mapped_column(Text)
     created_at: Mapped[str] = mapped_column(Text)
 
-    metrics: Mapped[List['Metrics']] = relationship('Metrics', back_populates='devices')
-
-
-class MetricTypes(Base):
-    __tablename__ = 'metric_types'
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
-    type: Mapped[str] = mapped_column(Text, unique=True)
-    created_at: Mapped[str] = mapped_column(Text)
-
-    metrics: Mapped[List['Metrics']] = relationship('Metrics', back_populates='metric_types')
+    devices: Mapped[List['Devices']] = relationship('Devices', back_populates='aggregator')
 
 
 class Visits(Base):
@@ -38,14 +29,52 @@ class Visits(Base):
     last_visit: Mapped[datetime.datetime] = mapped_column(DateTime)
 
 
-class Metrics(Base):
-    __tablename__ = 'metrics'
+class Devices(Base):
+    __tablename__ = 'devices'
 
-    device: Mapped[int] = mapped_column(ForeignKey('devices.id'))
-    type: Mapped[int] = mapped_column(ForeignKey('metric_types.id'))
-    value: Mapped[decimal.Decimal] = mapped_column(Numeric)
+    device_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    device_uuid: Mapped[str] = mapped_column(Text, unique=True)
+    device_name: Mapped[str] = mapped_column(Text)
+    aggregator_id: Mapped[int] = mapped_column(ForeignKey('aggregators.aggregator_id'))
     created_at: Mapped[str] = mapped_column(Text)
-    id: Mapped[Optional[int]] = mapped_column(Integer, primary_key=True)
 
-    devices: Mapped['Devices'] = relationship('Devices', back_populates='metrics')
-    metric_types: Mapped['MetricTypes'] = relationship('MetricTypes', back_populates='metrics')
+    aggregator: Mapped['Aggregators'] = relationship('Aggregators', back_populates='devices')
+    metric_snapshots: Mapped[List['MetricSnapshots']] = relationship('MetricSnapshots', back_populates='device')
+    metric_types: Mapped[List['MetricTypes']] = relationship('MetricTypes', back_populates='device')
+
+
+class MetricSnapshots(Base):
+    __tablename__ = 'metric_snapshots'
+
+    metric_snapshot_id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
+    device_id: Mapped[int] = mapped_column(ForeignKey('devices.device_id'))
+    client_timestamp_utc: Mapped[str] = mapped_column(Text)
+    client_timezone_minutes: Mapped[int] = mapped_column(Integer)
+    server_timestamp_utc: Mapped[str] = mapped_column(Text)
+    server_timezone_minutes: Mapped[int] = mapped_column(Integer)
+
+    device: Mapped['Devices'] = relationship('Devices', back_populates='metric_snapshots')
+    metric_values: Mapped[List['MetricValues']] = relationship('MetricValues', back_populates='metric_snapshot')
+
+
+class MetricTypes(Base):
+    __tablename__ = 'metric_types'
+
+    metric_type_id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
+    device_id: Mapped[int] = mapped_column(ForeignKey('devices.device_id'))
+    metric_type_name: Mapped[str] = mapped_column(Text)
+    created_at: Mapped[Optional[str]] = mapped_column(Text)
+
+    device: Mapped['Devices'] = relationship('Devices', back_populates='metric_types')
+    metric_values: Mapped[List['MetricValues']] = relationship('MetricValues', back_populates='metric_type')
+
+
+class MetricValues(Base):
+    __tablename__ = 'metric_values'
+
+    metric_snapshot_id: Mapped[int] = mapped_column(ForeignKey('metric_snapshots.metric_snapshot_id'), primary_key=True)
+    metric_type_id: Mapped[int] = mapped_column(ForeignKey('metric_types.metric_type_id'), primary_key=True)
+    value: Mapped[decimal.Decimal] = mapped_column(Numeric)
+
+    metric_snapshot: Mapped['MetricSnapshots'] = relationship('MetricSnapshots', back_populates='metric_values')
+    metric_type: Mapped['MetricTypes'] = relationship('MetricTypes', back_populates='metric_values')
